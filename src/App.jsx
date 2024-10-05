237f58591fb2ed8b1cce284853637c9f1f2ba869
saramargarido <saramargarido@gmail.com>
2024-10-05 20:03:05 +0200

    create and customizing sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from "./App.module.css"
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
 
   return (
-    <>
-      <Header />  
-    </>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi necessitatibus sed dicta dolor veritatis saepe, expedita soluta nostrum ut mollitia earum, eius non quia a, nihil corrupti quos ratione voluptate?
+      </main>  
+      </div>  
+    </div>
   )
 }
 
