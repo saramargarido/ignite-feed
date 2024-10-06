f101f9f3ae847882eb3e30453d99643ff056156f
saramargarido <saramargarido@gmail.com>
2024-10-06 18:15:13 +0200

    Create Avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,12 @@
+import styles from "./Avatar.module.css";
+
+// eslint-disable-next-line react/prop-types
+export function Avatar({ hasBorder = true, src }) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={src}
+      alt="Avatar do usuário"
+    />
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,8 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="http://github.com/saramargarido.png"
-            alt="Avatar do usuário"
-          />
+          <Avatar src="https://github.com/saramargarido.png" />
+
           <div className={styles.authorInfo}>
             <strong>Sara Margarido</strong>
             <span>Frontend Developer</span>
