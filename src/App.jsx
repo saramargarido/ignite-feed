02c527be90d5f327f7541c56c7e6c5a60c201848
saramargarido <saramargarido@gmail.com>
2024-10-06 17:08:19 +0200

    create and styling Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { Header } from './components/Header'
-import './global.css'
-import styles from "./App.module.css"
-import { Sidebar } from './components/Sidebar'
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 export function App() {
-
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi necessitatibus sed dicta dolor veritatis saepe, expedita soluta nostrum ut mollitia earum, eius non quia a, nihil corrupti quos ratione voluptate?
-      </main>  
-      </div>  
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
-
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="http://github.com/saramargarido.png"
+            alt="Avatar do usuário"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Sara Margarido</strong>
+            <span>Frontend Developer</span>
+          </div>
+        </div>
+
+        <time title="06 de outubro às 15h10" dateTime="2024-10-06 15:10:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>{" "}
+        </p>
+      </div>
+    </article>
+  );
+}
